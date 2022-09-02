017b3b9660926f4c6c8b60432f42695bcf4033d8
Nilakshhh <97288540+Nilakshhh@users.noreply.github.com>
2022-09-02 19:17:31 +0530

    initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Note from "./Note";
+
+function page() {
+    return (<div>
+        <Header />
+        <Footer />
+        <Note />
+        </div>
+    )
+}
+export default page;
